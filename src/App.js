7806172d8f6ae0926f667d15ab9499e729e125d9
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2020-11-25 23:27:42 +0530

    Testimonials

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import HomePage from './Components/HomePage';
 import Footer from './Components/Footer';
+<<<<<<< Updated upstream
+=======
+import Testimonials from './Components/Testimonials';
+import TalkingToFriendsSeniors from './Components/TalkingToFriendsSeniors'
+import Login from './Components/Login';
+import firebase from 'firebase';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { useStateValue } from './StateProvider';
+import { actionTypes } from './reducer';
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <Navbar/>
       <HomePage/>
       <Footer/>
+=======
+    <div className="app">
+      {/* <img src="login_bg.jpeg" alt='Random Image'/> */}
+      <Router>
+        {/* {!currentUser? 
+          (
+            <Login/>
+            ) : ( */}
+            <div>
+              <Navbar/>
+              <Switch>
+                <Route path='/friends'>
+                  <TalkingToFriendsSeniors/>
+                </Route>
+                <Route path='/test'>
+                  <Testimonials/>
+                </Route>
+                <Route path='/'>
+                  <div className='app__body'>
+                    <HomePage/>
+                  </div>
+                </Route>
+              </Switch>
+              <Footer/>
+            </div>
+        {/* )}  */}
+      </Router>
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 function HomePage() {
@@ -5,6 +6,62 @@ function HomePage() {
         <div>
             <p>Hello Faccho</p>
         </div>
+=======
+import React from 'react';
+import firebase from 'firebase';
+import { useStateValue } from '../StateProvider';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import tanjiro from '../Assets/tanjiro.jpg'
+
+import Marquee from "react-marquee-slider";
+import times from "lodash/times";
+
+
+function HomePage() {
+
+    const [{user, isSignedIn, userName}] = useStateValue()
+
+    // const Reviews = () => (
+    //     <Marquee key={key} velocity={25} scatterRandomly minScale={0.7}>
+    //       {times(5, String).map(id => (
+    //         <Box key={`marquee-example-review-${id}`} scale={scale}>
+    //           <Review scale={scale}>
+    //             <Avatar scale={scale}>
+    //               <img src="https://randomuser.me/api/portraits/women/68.jpg" />
+    //             </Avatar>
+    //             <Content scale={scale}>
+    //               <p>
+    //                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    //                 incididunt ut labore et dolore magna aliqua.
+    //               </p>
+    //             </Content>
+    //           </Review>
+    //         </Box>
+    //       ))}
+    //     </Marquee>
+    //   );
+      
+    return (
+        <div className='homePage'>
+            <Card className='homePageCard' variant="outlined">
+                <CardContent>
+                    <Typography className='textCard' color="textSecondary" gutterBottom>
+                    change of College Life
+                    </Typography> 
+                </CardContent>
+            </Card>
+        </div>
+
+        
+
+        // <div>
+        //     {/* <h1>Hello {user?.displayName}!!</h1> */}
+        //     {/* <h1>Hello {userName}!!</h1> */}
+        //     {/* <h1>Hello {firebase.auth().currentUser.displayName}!!</h1> */}            
+        // </div>
+>>>>>>> Stashed changes
     )
 }
 
