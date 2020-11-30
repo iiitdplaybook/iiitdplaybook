68fadc451f5eb648adb6f828745fe332715635e1
PabitraBansal <bansalpabitra@hotmail.com>
2020-11-30 17:28:33 +0300

    Explore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Components/HomePage";
 import Footer from "./Components/Footer";
@@ -14,51 +14,54 @@ import { useStateValue } from "./StateProvider";
 import Explore from "./Components/Explore";
 
 function App() {
-  const [{}, dispatch] = useStateValue();
+    const [{}, dispatch] = useStateValue();
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((authUser) => {
-      if (authUser) {
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
-          isSignedIn: true,
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+                dispatch({
+                    type: "SET_USER",
+                    user: authUser,
+                    isSignedIn: true,
+                });
+            } else {
+                dispatch({
+                    type: "SET_USER",
+                    user: null,
+                    isSignedIn: false,
+                });
+            }
         });
-      } else {
-        dispatch({
-          type: "SET_USER",
-          user: null,
-          isSignedIn: false,
-        });
-      }
-    });
-  }, []);
+    }, []);
 
-  return (
-    <div className="app">
-      {/* <img src="login_bg.jpeg" alt='Random Image'/> */}
-      <Router>
-        {!localStorage.getItem("isSignedIn") ? (
-          <Login />
-        ) : (
-          <div>
-            <Navbar />
-            <Switch>
-              <Route path="/explore" component={Explore} />
-              <Route path="/friends" component={TalkingToFriendsSeniors} />
-              <Route path="/test" component={Testimonials} />
-              <Route path="/">
-                <div className="app__body">
-                  <HomePage />
-                </div>
-              </Route>
-            </Switch>
-            <Footer />
-          </div>
-        )}
-      </Router>
-    </div>
-  );
+    return (
+        <div className="app">
+            {/* <img src="login_bg.jpeg" alt='Random Image'/> */}
+            <Router>
+                {!localStorage.getItem("isSignedIn") ? (
+                    <Login />
+                ) : (
+                    <div>
+                        <Navbar />
+                        <Switch>
+                            <Route path="/explore" component={Explore} />
+                            <Route
+                                path="/friends"
+                                component={TalkingToFriendsSeniors}
+                            />
+                            <Route path="/test" component={Testimonials} />
+                            <Route path="/">
+                                <div className="app__body">
+                                    <HomePage />
+                                </div>
+                            </Route>
+                        </Switch>
+                        <Footer />
+                    </div>
+                )}
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,17 +6,21 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 function HomePage() {
-  return (
-    <div className="homePage">
-      <Card className="homePageCard" variant="outlined">
-        <CardContent>
-          <Typography className="textCard" color="textSecondary" gutterBottom>
-            Phases of College Life
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
-  );
+    return (
+        <div className="homePage">
+            <Card className="homePageCard" variant="outlined">
+                <CardContent>
+                    <Typography
+                        className="textCard"
+                        color="textSecondary"
+                        gutterBottom
+                    >
+                        Phases of College Life
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    );
 }
 
 export default HomePage;
