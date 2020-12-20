d6b79d425ffd26d5b644028fc19867b2737f3211
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2020-12-21 05:02:19 +0530

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,10 @@ function Footer() {
     
     return (
         <div id="bg">
-            {/* <p>Want to contribute? It can as small as 2 lines or big as 2 pages. Your contribution matters</p> */}
+            <div className="upper">
+                <p>Want to contribute? It can as small as 2 lines or big as 2 pages. Your contribution matters</p>
+                
+            </div>
             <div className="lower">
                 <p>
                 IIITD Student Playbook &copy; 2021
