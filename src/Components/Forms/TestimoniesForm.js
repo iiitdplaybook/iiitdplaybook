6b983401673de519f9e4471e9ff6e951333192d4
Rahul Singh <31858177+B1u3B01t@users.noreply.github.com>
2020-12-28 05:38:42 +0530

    testimonies

--- src/Components/Forms/TestimoniesForm.js
+++ src/Components/Forms/TestimoniesForm.js
@@ -1,44 +1,117 @@
 import firebase from 'firebase';
 import React from 'react';
 import Navbar from "../Navbar";
+import { makeStyles } from '@material-ui/core/styles';
+import { useStateValue } from '../../StateProvider';
+import './TestimoniesForm.css'
+// Material UI for Form
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Button from '@material-ui/core/Button';
 
 const state = {
     "UserAvatar": "https://www.gstatic.com/stadia/gamers/avatars/xxhdpi/avatar_53.png",
     "Name":"Rahul Singh",
     "Text":"",
+    "Topic":"",
     "isApproved":false
 };
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+  
 
 export default function TestimoniesForm(){
 
-    function updateTestimonies(event){
-        state.Testimonies = event.target.value
-        console.log(`updated testies ${state.Testimonies}`)
-    }
+    const [{user, isSignedIn, userName}] = useStateValue()
+
     function submitForm(event){
         event.preventDefault();
+        state.Topic=value;
+        state.Name=userName;
+        state.Text=testimonies;
+        state.UserAvatar = firebase.auth().currentUser.photoURL;
         const db = firebase.firestore();
         console.log("Sending to Firebase");
         db.collection("Testimonies").add(state);
+        settestimonies('');
     }
 
+    const classes = useStyles();
 
-    
+    const [value, setValue] = React.useState('General');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const [testimonies, settestimonies] = React.useState('');
+    const handleTestieChange = (event) =>{
+        settestimonies(event.target.value)
+    };
+
     return(
         <div>
             <Navbar loggedIn={true} colorStatus={true}/>
-            <form onSubmit={submitForm}>
-                <input
-                    type="text"
-                    name="testimonies"
-                    placeholder="testimonies"
-                    // onChange={updateTestimonies}
-                />
-                <button
-                type="submit">
-                    Submit
-                </button>
+            <form className={classes.root} noValidate autoComplete="off">
+                <div className="formdiv">
+                    <h1 className='formlabel'>For which part would you like to give your testimonies?</h1>
+                    <FormControl component="fieldset">
+                    {/* <FormLabel className='formlabel' component="legend">For which part would you like to give your testimonies?</FormLabel> */}
+                    <RadioGroup className='radio' aria-label="testimonies" name="testimonies1" value={value} onChange={handleChange}>
+                        <FormControlLabel value="Clubs" control={<Radio />} label="Clubs" />
+                        <FormControlLabel value="Time Management" control={<Radio />} label="Time Management" />
+                        <FormControlLabel value="Online Semester Tips" control={<Radio />} label="Online Semester Tips" />
+                        <FormControlLabel value="Academics" control={<Radio />} label="Academics" />
+                        <FormControlLabel value="Competitive Programming" control={<Radio />} label="Competitive Programming" />
+                        <FormControlLabel value="Hackathons" control={<Radio />} label="Hackathons" />
+                        <FormControlLabel value="Research" control={<Radio />} label="Research" />
+                        <FormControlLabel value="Placements" control={<Radio />} label="Placements" />
+                        <FormControlLabel value="Images and Experiences" control={<Radio />} label="Images and Experiences" />
+                    </RadioGroup>
+                    <FormHelperText className='helperlabel'>Choose one of the following and submit, you can submit the form again for filling for another section</FormHelperText>
+                    </FormControl>
+                    <br/>
+                    <br/>
+                    <TextField
+                    disabled
+                    id="outlined-disabled"
+                    label="Name"
+                    defaultValue={userName}
+                    variant="outlined"
+                    className='nameLabel'
+                    color="secondary"
+                    />
+                    <br/>
+                    <TextField
+                    id="outlined-textarea"
+                    label="Your Testimonials"
+                    placeholder="Share your Insights with others"
+                    multiline
+                    variant="outlined"
+                    className='testimonials'
+                    color="secondary"
+                    value={testimonies}
+                    onChange={handleTestieChange}
+                    />
+                    <br/>
+                    <p>Disclaimer: Your ID's profile picture will be displayed along with Name and your Testimony</p>
+                    <br/>
+                    <Button className='button' variant="contained" color="secondary" onClick={submitForm}>
+                        Submit
+                    </Button>
+                </div>
             </form>
         </div>
     );
