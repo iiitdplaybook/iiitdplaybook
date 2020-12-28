a403f06fbd1c2beb33e3e005bd56c40bf4d9ce2d
Rahul Singh <31858177+B1u3B01t@users.noreply.github.com>
2020-12-28 15:27:37 +0530

    Updates to Testimonies

--- src/Components/Forms/TestimoniesForm.js
+++ src/Components/Forms/TestimoniesForm.js
@@ -12,6 +12,10 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
 import Button from '@material-ui/core/Button';
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -44,9 +48,16 @@ export default function TestimoniesForm(){
       });
     const [{user, isSignedIn, userName}] = useStateValue()
 
+    // const [userName, setUserName] = React.useState('');
+
     function submitForm(event){
         event.preventDefault();
-        state.Topic=value;
+        if(value=="Phases of College") {
+            state.Topic=value+' - '+PoC_Value; 
+        }
+        else{
+            state.Topic=value;
+        }
         state.Name=userName;
         state.Text=testimonies;
         state.UserAvatar = firebase.auth().currentUser.photoURL;
@@ -57,6 +68,10 @@ export default function TestimoniesForm(){
         settestimonies('');
     }
 
+    function showError(){
+        toast.error("Exceeded Word Limit in Testimonies")
+    }
+
     const classes = useStyles();
 
     const [value, setValue] = React.useState('General');
@@ -64,11 +79,45 @@ export default function TestimoniesForm(){
     const handleChange = (event) => {
         setValue(event.target.value);
     };
+    React.useEffect(
+        ()=>{
+            if(value==="Clubs"){
+                setHint(clubHint);
+            }
+            else if(value==="Time Management"){
+                setHint(timeManagementHint);
+            }
+            else if(value==="Online Semester Tips"){
+                setHint(onlineSemHint);
+            }
+            else {
+                setHint(defaultHint)
+            }
+        });
+
 
     const [testimonies, settestimonies] = React.useState('');
     const handleTestieChange = (event) =>{
         settestimonies(event.target.value)
     };
+
+    const [PoC_Value, setPoC_Value] = React.useState('');
+    const PoC_ValueChange = (event)=>{
+        setPoC_Value(event.target.value)
+    }
+
+    const [hint, setHint] = React.useState("Share your Insights with others");
+    const [word_count, setword_count] = React.useState(0);
+
+    React.useEffect(()=>{
+        // console.log('word entry')
+        setword_count(testimonies.length)
+    }, [testimonies])
+
+    const defaultHint = "Share your Insights with others"
+    const clubHint = "Which club are you a part of? Which club helped you grow as person? In what ways did the club help?"
+    const timeManagementHint = "What are your tried and tested techniques to be more productive? How do you manage time? How do you balance life/fun and deadlines?"
+    const onlineSemHint = "Any tips for the online semester? How are you coping with the online semester? Any stories or feelings that you'd like share?"
 
     return(
         <div>
@@ -80,6 +129,7 @@ export default function TestimoniesForm(){
                     {/* <FormLabel className='formlabel' component="legend">For which part would you like to give your testimonies?</FormLabel> */}
                     <RadioGroup className='radio' aria-label="testimonies" name="testimonies1" value={value} onChange={handleChange}>
                         <FormControlLabel className='radioButtonLeft' value="General" control={<Radio />} label="General" />
+                        <FormControlLabel value="Phases of College" control={<Radio />} label="Phases of College" />
                         <FormControlLabel className='radioButtonRight' value="Clubs" control={<Radio />} label="Clubs" />
                         <FormControlLabel value="Time Management" control={<Radio />} label="Time Management" />
                         <FormControlLabel value="Online Semester Tips" control={<Radio />} label="Online Semester Tips" />
@@ -94,6 +144,25 @@ export default function TestimoniesForm(){
                     </FormControl>
                     <br/>
                     <br/>
+                    <br/>
+                    {(value==="Phases of College")?
+                    <FormControl className='nameLabel' color="secondary">
+                        <InputLabel id="demo-simple-select-filled-label">Phases of College</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-filled-label"
+                        id="demo-simple-select-filled"
+                        value={PoC_Value}
+                        onChange={PoC_ValueChange}>
+                            <MenuItem value="Baby Steps">Baby Steps</MenuItem>
+                            <MenuItem value='Exploring'>Exploring</MenuItem>
+                            <MenuItem value='Defining Point'>Defining Point</MenuItem>
+                            <MenuItem value='Graduating'>Graduating</MenuItem>
+                            <MenuItem value='Nostalgia'>Nostalgia</MenuItem>
+                        </Select>
+                    </FormControl>
+                    :null}
+                    <br/>
+                    <br/>
                     <TextField
                     disabled
                     id="outlined-disabled"
@@ -106,8 +175,8 @@ export default function TestimoniesForm(){
                     <br/>
                     <TextField
                     id="outlined-textarea"
-                    label="Your Testimony"
-                    placeholder="Share your Insights with others"
+                    label={`Your Testimony  (${word_count}/400)`}
+                    placeholder={hint}
                     multiline
                     variant="outlined"
                     className='testimonials'
@@ -118,7 +187,7 @@ export default function TestimoniesForm(){
                     <br/>
                     <p>Disclaimer: Your ID's profile picture will be displayed along with Name and your Testimony</p>
                     <br/>
-                    <Button className='button' variant="contained" color="secondary" onClick={submitForm}>
+                    <Button className='button' variant="contained" color="secondary" onClick={(word_count<=400)?submitForm:showError}>
                         Submit
                     </Button>
                 </div>
