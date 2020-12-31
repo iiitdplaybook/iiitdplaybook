a88f40ba0d68c9996638a229f043c7299cea4eb2
Rahul Singh <31858177+B1u3B01t@users.noreply.github.com>
2020-12-31 15:51:26 +0530

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -10,11 +10,35 @@ var firebaseConfig = {
     appId: "1:557827916584:web:679950ab0a34089436461d",
     measurementId: "G-5BBSPSTEEE"
   };
+
+// firebaseConfig = {
+//   apiKey: "AIzaSyB4Dh-q8PPvW-oQGjLo97VEmSAfsjRLAJk",
+//   authDomain: "iiitd-playbook-testing.firebaseapp.com",
+//   projectId: "iiitd-playbook-testing",
+//   storageBucket: "iiitd-playbook-testing.appspot.com",
+//   messagingSenderId: "519422920935",
+//   appId: "1:519422920935:web:3351648e10724e9d373a42",
+//   measurementId: "G-FT1VNCWVW3"
+// };
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider().setCustomParameters({ hd: "iiitd.ac.in" });
 firebase.analytics();
+
+firebase.firestore().enablePersistence()
+  .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          console.log("Multiple tabs open, persistence can only be enabled")
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          console.log("The current browser does not support all of the")
+          // features required to enable persistence
+          // ...
+      }
+  });
+// Subsequent queries will use persistence, if it was enabled successfully
 
 export default fire;
 export {auth, provider};
