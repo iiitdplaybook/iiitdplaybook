072ad955df7a21a713b6993d9f711141938289c1
PabitraBansal <bansalpabitra@hotmail.com>
2021-04-04 13:24:41 +0300

    QnA fetch from firebase

--- src/Components/QnA/QnA.js
+++ src/Components/QnA/QnA.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Navbar from '../Navbar/Navbar';
 import './QnA.css'
 import firebase from 'firebase';
+import QnACardGrid from './QnACardGrid'
 
 function QnA() {
 
@@ -15,21 +16,21 @@ function QnA() {
             let childList = [];
             const dict = {};
             dict.question = child.val().question;
-            // child.answers.forEach((children) => {
-            //     const childDict = {};
-            //     childDict.link = children.val().link;
-            //     childDict.image = children.val().image;
-            //     childDict.heading = children.val().heading;
-            //     childDict.verified = children.val().verified;
-            //     childDict.key = children.key;
-            //     childList.push(childDict);
-            // });
-            // dict.children = childList;
             dict.key = child.key;
+            for (let k in child.val().answers){
+                const childDict = {};
+                childDict.link = child.val().answers[k].link;
+                childDict.image = child.val().answers[k].image;
+                childDict.heading = child.val().answers[k].heading;
+                childDict.description = child.val().answers[k].description;
+                childDict.verified = child.val().answers[k].verified;
+                childDict.key = k;
+                childList.push(childDict);
+            };
+            dict.children = childList;
             items.push(dict);
           });
           setQna(items);
-          console.log(items);
         });
       };
     
@@ -40,7 +41,9 @@ function QnA() {
     return (
         <div className='qna'>
             <Navbar loggedIn={true} colorStatus={true} />
-            
+            <div className='cardsDiv_qna'>
+            <QnACardGrid itemList={qna} />
+          </div>
         </div>
     )
 }
