25b829aad9c8b143f1b2e7a07b13f272772370f2
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-04-09 21:20:37 +0530

    New footer style

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Creators from './Creators';
 import './Footer.css';
 import { Button } from '@material-ui/core';
@@ -6,52 +6,42 @@ import { Link } from 'react-router-dom';
 import TeamMembers from './Team';
 
 function Footer({ page }) {
+  let signedIn = true;
+
+  useEffect(() => {
+    signedIn = localStorage.getItem('isSignedIn');
+  });
+
   return (
     <div id={page === 'login' ? 'footer-login' : 'footer'}>
       <div className='upper'>
-        {localStorage.getItem('isSignedIn') ? (
-          <>
-            <p>
-              Want to contribute? It can as small as 2 lines or big as 2 pages.
-              Your contribution matters
-            </p>
-
+        <div className='divider'>
+          <div className='support-us'>
+            <div>
+              <Button
+                id='supportBtn'
+                target='_blank'
+                href='https://www.buymeacoffee.com/iiitdplaybook'
+              >
+                <img
+                  className='buymeacoffeelogo'
+                  src={
+                    'https://bmc-dev.s3.us-east-2.amazonaws.com/assets/icons/bmc_icon_black.png'
+                  }
+                />{' '}
+                Support Us
+              </Button>
+            </div>
+          </div>
+          <div className='contribute'>
             <Button
               className='button2'
               component={Link}
               to={'/contribute/testimonies'}
             >
-              <span className='span2'>YES! I'd like to contribute</span>
+              <span className='span2'>Contribute</span>
             </Button>
-          </>
-        ) : (
-          <></>
-        )}
-        <p
-          className={
-            page === 'login' ? 'support-us-text-login' : 'support-us-text'
-          }
-        >
-          Support Us
-        </p>
-        <div
-          className={
-            page === 'login' ? 'supportUsButtons-login' : 'supportUsButtons'
-          }
-        >
-          <Button
-            id='supportBtn'
-            target='_blank'
-            href='https://www.buymeacoffee.com/iiitdplaybook'
-          >
-            <img
-              className='buymeacoffeelogo'
-              src={
-                'https://bmc-dev.s3.us-east-2.amazonaws.com/assets/icons/bmc_icon_black.png'
-              }
-            />{' '}
-            Buy me a coffee
-          </Button>
+          </div>
         </div>
         <div className='creators'>
           <Creators />
