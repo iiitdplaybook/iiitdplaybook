114dba3b84819a775850a48aec11212049acf361
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-04-10 13:34:25 +0530

    Modify footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -19,7 +19,7 @@ function Footer({ page }) {
           <div className='support-us'>
             <div>
               <Button
-                id='supportBtn'
+                className='footerBtn'
                 target='_blank'
                 href='https://www.buymeacoffee.com/iiitdplaybook'
               >
@@ -28,18 +28,25 @@ function Footer({ page }) {
                   src={
                     'https://bmc-dev.s3.us-east-2.amazonaws.com/assets/icons/bmc_icon_black.png'
                   }
-                />{' '}
+                />
                 Support Us
               </Button>
             </div>
           </div>
           <div className='contribute'>
             <Button
-              className='button2'
+              className='footerBtn'
+              // className='button2'
               component={Link}
               to={'/contribute/testimonies'}
             >
-              <span className='span2'>Contribute</span>
+              <img
+                className='buymeacoffeelogo'
+                src={
+                  'https://www.flaticon.com/svg/vstatic/svg/2057/2057748.svg?token=exp=1618041325~hmac=3ddd900cabe3b6220bb3ef682e1a15bb'
+                }
+              />
+              Contribute
             </Button>
           </div>
         </div>
@@ -51,7 +58,7 @@ function Footer({ page }) {
         </div>
       </div>
       <div className={page === 'login' ? 'lower-login' : 'lower'}>
-        <p>IIITD Student Playbook &copy; 2021</p>
+        IIITD Student Playbook &copy; 2021
       </div>
     </div>
   );
