4669352c0ebfb802a23ef65ce3c6b2fa39eb8b0f
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-04-20 18:52:33 +0530

    Fix illustration links

--- src/Components/Internships/Metadata.js
+++ src/Components/Internships/Metadata.js
@@ -9,35 +9,35 @@ export const metadata = {
     title: 'Introduction',
     // path: '/internships/introduction',
     image:
-      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c466bcf47f9595f6d97d37788497ae92339cb16a/src/Assets/Internships/introduction.svg',
+      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c92163f6/src/Assets/Internships/introduction.svg',
     component: Introduction,
   },
   resume: {
     title: 'Build Your Resume',
     // path: '/internships/resume',
     image:
-      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c466bcf47f9595f6d97d37788497ae92339cb16a/src/Assets/Internships/resume.svg',
+      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c92163f6/src/Assets/Internships/resume.svg',
     component: Resume,
   },
   preparation: {
     title: 'Preparation Guide',
     // path: '/internships/hall-of-fame',
     image:
-      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c466bcf47f9595f6d97d37788497ae92339cb16a/src/Assets/Internships/preparation.svg',
+      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c92163f6/src/Assets/Internships/preparation.svg',
     component: Preparation,
   },
   interviewTips: {
     title: 'Interview Tips',
     // path: '/internships/interview-tips',
     image:
-      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c466bcf47f9595f6d97d37788497ae92339cb16a/src/Assets/Internships/interview_tips.svg',
+      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c92163f6/src/Assets/Internships/interview_tips.svg',
     component: InterviewTips,
   },
   experience: {
     title: 'Internship Experience',
     // path: '/internships/experience',
     image:
-      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c466bcf47f9595f6d97d37788497ae92339cb16a/src/Assets/Internships/experience.svg',
+      'https://cdn.statically.io/gh/ananyalohani/iiitdplaybook/c92163f6/src/Assets/Internships/experience.svg',
     component: Experience,
   },
 };
