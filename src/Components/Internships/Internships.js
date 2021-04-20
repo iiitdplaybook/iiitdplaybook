c466bcf47f9595f6d97d37788497ae92339cb16a
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-04-20 16:49:58 +0530

    Add internship page illustrations

--- src/Components/Internships/Internships.js
+++ src/Components/Internships/Internships.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Internships.css';
 
 function Internships() {
-  return <div></div>;
+  return (
+    <div className='internship_page'>
+      <Navbar loggedIn={true} colorStatus={false} stickyCond={true} />
+      <div className='navigation'>
+        <h1 className='nav_heading'>One stop guide for your internships</h1>
+        <div className='nav_divider'>
+          <div className='nav_button_container'>
+            <button className='nav_button'>Introduction</button>
+            <button className='nav_button'>Getting Started</button>
+            <button className='nav_button'>Interview Tips</button>
+            <button className='nav_button'>Build Your Resume</button>
+            <button className='nav_button'>Internship Experience</button>
+          </div>
+          <div className='nav_illustration'>
+            <img src={''} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Internships;
