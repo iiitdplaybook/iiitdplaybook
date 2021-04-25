93e77876d43be78da6850683527d68470ab99972
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-04-25 19:00:44 +0530

    Add Research vs Internship cards

--- src/Components/Internships/Pages/Introduction/Introduction.js
+++ src/Components/Internships/Pages/Introduction/Introduction.js
@@ -1,13 +1,72 @@
-import React from 'react';
+
+import React, { useEffect, useRef } from 'react';
 import '../global.css';
+import './Introduction.css';
 
 const Introduction = () => {
+  const sectionRef = useRef(null);
+  const top = 0;
+  const right = 0;
+
+  useEffect(() => {
+    top = sectionRef.current.offsetHeight / 2;
+    right = sectionRef.current.offsetWidth / 2;
+  })
+
   return (
-    <div>
-      <h1>Why Internship?</h1>
-      <h1>Rules and Regulations</h1>
-      <h1>Is CP important?</h1>
-      <div></div>
+    <div className='introduction'>
+      <h1 className='heading'>Why Internship?</h1>
+      <h1 className='heading'>Rules and Regulations</h1>
+      <h1 className='heading'>Is CP important?</h1>
+      <div className='research_vs_internship' ref={sectionRef}>
+        <div className='card'>
+          <h1>Research</h1>
+          <ul>
+            <li>
+              Aliquip ex voluptate ullamco amet anim cupidatat culpa non nulla
+              cillum aliquip deserunt adipisicing. Exercitation veniam enim
+              pariatur pariatur minim commodo ad esse deserunt consectetur elit
+              occaecat ad.
+            </li>
+            <li>
+              Aliquip ex voluptate ullamco amet anim cupidatat culpa non nulla
+              cillum aliquip deserunt adipisicing. Exercitation veniam enim
+              pariatur pariatur minim commodo ad esse deserunt consectetur elit
+              occaecat ad.
+            </li>
+            <li>
+              Aliquip ex voluptate ullamco amet anim cupidatat culpa non nulla
+              cillum aliquip deserunt adipisicing. Exercitation veniam enim
+              pariatur pariatur minim commodo ad esse deserunt consectetur elit
+              occaecat ad.
+            </li>
+          </ul>
+        </div>
+        <div className='vs'>v/s</div>
+        <div className='card'>
+          <h1>Internship</h1>
+          <ul>
+            <li>
+              Aliquip ex voluptate ullamco amet anim cupidatat culpa non nulla
+              cillum aliquip deserunt adipisicing. Exercitation veniam enim
+              pariatur pariatur minim commodo ad esse deserunt consectetur elit
+              occaecat ad.
+            </li>
+            <li>
+              Aliquip ex voluptate ullamco amet anim cupidatat culpa non nulla
+              cillum aliquip deserunt adipisicing. Exercitation veniam enim
+              pariatur pariatur minim commodo ad esse deserunt consectetur elit
+              occaecat ad.
+            </li>
+            <li>
+              Aliquip ex voluptate ullamco amet anim cupidatat culpa non nulla
+              cillum aliquip deserunt adipisicing. Exercitation veniam enim
+              pariatur pariatur minim commodo ad esse deserunt consectetur elit
+              occaecat ad.
+            </li>
+          </ul>
+        </div>
+      </div>
       <div></div>
     </div>
   );
