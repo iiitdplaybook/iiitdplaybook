ae10486c3192e41f934bdbb6ad4dc7f137a249a8
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-05-04 14:10:26 +0530

    Add basic image article cards

--- src/Components/Internships/Pages/Experience/Experience.js
+++ src/Components/Internships/Pages/Experience/Experience.js
@@ -1,11 +1,132 @@
 import React from 'react';
 import PageFooter from '../../PageFooter';
 import { metadata } from '../../Metadata';
+import './Experience.css';
 import '../global.css';
 
 const Experience = ({ callback }) => {
+  const articles = [
+    {
+      type: 'article',
+      url:
+        'https://www.notion.so/Playbook-team-5e39c62938ce4614aa9b8499fa7c1949',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Adobe'],
+      title: 'Life at Adobe - a remote experience',
+      author: 'Yashwin Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Microsoft', 'UI/UX'],
+      title: 'Life at Microsoft - a UX design remote experience',
+      author: 'Kshitij Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'article',
+      url:
+        'https://www.notion.so/Playbook-team-5e39c62938ce4614aa9b8499fa7c1949',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Adobe'],
+      title: 'Life at Adobe - a remote experience',
+      author: 'Yashwin Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Microsoft', 'UI/UX'],
+      title: 'Life at Microsoft - a UX design remote experience',
+      author: 'Kshitij Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'article',
+      url:
+        'https://www.notion.so/Playbook-team-5e39c62938ce4614aa9b8499fa7c1949',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Adobe'],
+      title: 'Life at Adobe - a remote experience',
+      author: 'Yashwin Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Microsoft', 'UI/UX'],
+      title: 'Life at Microsoft - a UX design remote experience',
+      author: 'Kshitij Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'article',
+      url:
+        'https://www.notion.so/Playbook-team-5e39c62938ce4614aa9b8499fa7c1949',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Adobe'],
+      title: 'Life at Adobe - a remote experience',
+      author: 'Yashwin Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Microsoft', 'UI/UX'],
+      title: 'Life at Microsoft - a UX design remote experience',
+      author: 'Kshitij Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+    {
+      type: 'article',
+      url:
+        'https://www.notion.so/Playbook-team-5e39c62938ce4614aa9b8499fa7c1949',
+      image: 'https://dummyimage.com/360x300/04ABB0/fff.png&text=article+image',
+      tags: ['Adobe'],
+      title: 'Life at Adobe - a remote experience',
+      author: 'Yashwin Agrawal',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...",
+    },
+  ];
+
   return (
-    <div className='experience' style={{ width: '85%', margin: '0 auto' }}>
+    <div
+      className='experience'
+      style={{ width: '85%', margin: '0 auto', position: 'relative' }}
+    >
+      <div className='article_grid'>
+        {articles.map((item) => {
+          return (
+            <div className='article_card'>
+              <a href={item.url}>
+                <img className='article_image' src={item.image} />
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  {item.tags.map((tag) => (
+                    <div className='article_tag'>{tag}</div>
+                  ))}
+                </div>
+                <h2 className='article_title'>{item.title}</h2>
+                <p className='article_author'>{item.author}</p>
+                <p className='article_desc'>{item.description}</p>
+              </a>
+            </div>
+          );
+        })}
+      </div>
       <PageFooter
         settings={{
           left: {
